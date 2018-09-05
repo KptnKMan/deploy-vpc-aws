98019e0500c9c5fe22c3f5790d2e67da95644805
Kareem Straker <kareem.straker@gmail.com>
2018-09-05 20:31:59 +0200

    cleanup script

---
+++ debug_cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Delete existing current certs
+rm -Rf config/ssl
+rm -Rf config/*.key
+rm -Rf config/*.pub
+rm -Rf config/*.ssh
+
+## Delete terraform state files
+rm -Rf config/cluster.state*
